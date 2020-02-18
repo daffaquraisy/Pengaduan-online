c69e8e1e11104a69df431bcb68afbb92dfcee00d
daffaquraisy <daffaquraisy@gmail.com>
2020-02-18 19:24:28 +0700

    first

---
+++ database/seeds/AdministratorSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdministratorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $administrator = new \App\User;
+        $administrator->username = "Admin";
+        $administrator->name = "Administrator";
+        $administrator->email = "admin@gmail.com";
+        $administrator->roles = json_encode(["ADMIN"]);
+        $administrator->password = \Hash::make("12345678");
+
+        $administrator->save();
+
+        $this->command->info("User Admin berhasil diinsert");
+    }
+}
